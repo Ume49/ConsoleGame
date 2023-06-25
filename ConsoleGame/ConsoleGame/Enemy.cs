119a46c009a1941195f8eddfc8e4ee78bb46c0cd
Ume49 <121100231+MetalTomoki@users.noreply.github.com>
2023-06-25 18:47:51 +0900

    敵の構造体

---
+++ ConsoleGame/ConsoleGame/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGame
+{
+    public struct Enemy
+    {
+        public int hp;
+        public int attack;
+        public int deffence;
+    }
+}
