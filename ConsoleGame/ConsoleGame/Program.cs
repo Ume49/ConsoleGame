3365c3d3b2c2eea0db1555030a34b1f7875af67f
Ume49 <121100231+MetalTomoki@users.noreply.github.com>
2023-06-25 20:41:12 +0900

    エネミー作成
    めんどいのでハードコーディング

--- ConsoleGame/ConsoleGame/Program.cs
+++ ConsoleGame/ConsoleGame/Program.cs
@@ -6,4 +6,20 @@ TextGenerater text_generater = new TextGenerater();
 
 EnemyManager enemy_manager = new EnemyManager();
 
+{
+    Enemy enemy_slime = new Enemy();
+    enemy_slime.name = "岩スライム";
+    enemy_slime.hp = 10;
+    enemy_slime.attack = 1;
+    enemy_slime.deffence = 2;
+    enemy_manager.AddEnemy(enemy_slime);
+
+    Enemy enemy_kobold = new Enemy();
+    enemy_kobold.name = "コボルド";
+    enemy_kobold.hp = 18;
+    enemy_kobold.attack = 3;
+    enemy_kobold.deffence = 0;
+    enemy_manager.AddEnemy(enemy_kobold);
+}
+
 text_generater.Refresh();
