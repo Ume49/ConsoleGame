9be7e544dc3cfa749f1f8ae6729c206b84f91096
Ume49 <121100231+MetalTomoki@users.noreply.github.com>
2023-06-25 21:04:07 +0900

    プレイヤーのコンストラクタ

--- ConsoleGame/ConsoleGame/Player.cs
+++ ConsoleGame/ConsoleGame/Player.cs
@@ -11,5 +11,12 @@ namespace ConsoleGame
         public string _name;
         public int _hp;
         public int _attack;
+
+        public Player()
+        {
+            _name   = String.Empty;
+            _hp     = 0;
+            _attack = 0;
+        }
     }
 }
