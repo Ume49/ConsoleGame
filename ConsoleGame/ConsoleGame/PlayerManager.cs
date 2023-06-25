3423fb0c0230f2358248d4a0f57b53dac8a35557
Ume49 <121100231+MetalTomoki@users.noreply.github.com>
2023-06-25 18:57:20 +0900

    プレイヤーを管理するクラス

---
+++ ConsoleGame/ConsoleGame/PlayerManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGame
+{
+    public class PlayerManager
+    {
+        List<Player> players;
+
+        public PlayerManager()
+        {
+            players = new List<Player>();
+        }
+    }
+}
