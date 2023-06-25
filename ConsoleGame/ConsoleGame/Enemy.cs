fd66c788722a1ff9affed15ca2995f1c0a96a346
Ume49 <121100231+MetalTomoki@users.noreply.github.com>
2023-06-25 21:03:57 +0900

    エネミーのコンストラクタ

--- ConsoleGame/ConsoleGame/Enemy.cs
+++ ConsoleGame/ConsoleGame/Enemy.cs
@@ -12,5 +12,13 @@ namespace ConsoleGame
         public int _hp;
         public int _attack;
         public int _deffence;
+
+        public Enemy()
+        {
+            _name       = string.Empty;
+            _hp         = 0;
+            _attack     = 0;
+            _deffence   = 0;
+        }
     }
 }
