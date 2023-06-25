28f9de837b6d93f6eec15e664a08061bbf2e0d2b
Ume49 <121100231+MetalTomoki@users.noreply.github.com>
2023-06-25 18:56:11 +0900

    エネミーを管理するクラス

---
+++ ConsoleGame/ConsoleGame/EnemyManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGame
+{
+    public class EnemyManager
+    {
+        List<Enemy> enemies;
+
+        public EnemyManager()
+        {
+            enemies = new List<Enemy>();
+        }
+    }
+}
