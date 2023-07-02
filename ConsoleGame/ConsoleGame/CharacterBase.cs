b5449836c4276ad8a80c19009ac54fdb85049bd1
Ume49 <121100231+MetalTomoki@users.noreply.github.com>
2023-07-02 12:56:17 +0900

    メンバ追加

--- ConsoleGame/ConsoleGame/CharacterBase.cs
+++ ConsoleGame/ConsoleGame/CharacterBase.cs
@@ -8,9 +8,16 @@ namespace ConsoleGame
 {
     public abstract class CharacterBase
     {
+        public int _id;
+        public string _name;
+        public int _max_hp;
+        public int _current_hp;
+        public int _attack;
+        public int _deffence;
+        public int _speed;
+
         public CharacterBase()
         {
-
         }
     }
 }
