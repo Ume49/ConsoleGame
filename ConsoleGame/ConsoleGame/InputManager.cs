3f54a9c23a04baa7cef4b1d5b861bcb9680998fe
Ume49 <121100231+MetalTomoki@users.noreply.github.com>
2023-07-09 01:15:37 +0900

    入力のフェーズ
    多分後でもうちょっと追加する

--- ConsoleGame/ConsoleGame/InputManager.cs
+++ ConsoleGame/ConsoleGame/InputManager.cs
@@ -2,6 +2,13 @@
 {
     public class InputManager : Interface.Output
     {
+        public enum Phase
+        {
+            What,
+            Who,
+        }
+
+        Phase _phase;
         public List<string> Output()
         {
             List<string> output = new List<string>();
