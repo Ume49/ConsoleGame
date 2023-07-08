ee0c0c31bf1ef7c932e7d347a59843cd246249d0
Ume49 <121100231+MetalTomoki@users.noreply.github.com>
2023-07-09 02:45:13 +0900

    入力用のコマンドクラス

---
+++ ConsoleGame/ConsoleGame/InputCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleGame
+{
+    public class InputCommand
+    {
+        public string _command;
+        public string _key;
+
+        public InputCommand(string command, string key)
+        {
+            _command = command;
+            _key = key;
+        }
+
+        public bool IsKeyValid(string input_key)
+        {
+            return input_key == _key;
+        }
+    }
+}
